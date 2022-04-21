cd91de8baa33f15fb112f2286ce15085a063ecaa
Arakawa Shintaro <arashinbe@gmail.com>
2022-04-21 14:52:17 +0900

    debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])
@@ -6,6 +7,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -31,15 +33,19 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body).merge(user_id: current_user.id)
+  end
+
+  def ensure_correct_user
+    redirect_to books_path unless Book.find(params[:id]).user == current_user
   end
 end
